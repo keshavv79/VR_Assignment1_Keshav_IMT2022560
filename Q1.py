3710d51915ead781fa97832b3931a37fe9bbc4ed
Keshav Goyal <116884967+keshavv79@users.noreply.github.com>
2025-02-21 14:54:24 +0530

    Update Q1.py

--- Q1.py
+++ Q1.py
@@ -2,9 +2,12 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-image = cv2.imread('Question1Images/img1.jpg')
+image = cv2.imread('Question1Images/img5.jpg')
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 blur = cv2.GaussianBlur(gray, (15, 15), 7)
+plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+plt.title("Original Image")
+plt.show()
 
 _, binary = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 kernel = np.ones((3, 3), np.uint8)
@@ -15,7 +18,7 @@ edges = cv2.Canny(binary, 120, 250)
 plt.imshow(edges, cmap='gray')
 plt.title("Edge detection of coins")
 plt.show()
-cv2.imwrite("edge_img1.jpg", edges) 
+cv2.imwrite("Question1Outputs/EdgeImg5.jpg", edges) 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
@@ -27,7 +30,7 @@ cv2.drawContours(segmented, contours, -1, 255, thickness=cv2.FILLED)
 plt.imshow(segmented, cmap='gray')
 plt.title("Segmentation of coins")
 plt.show()
-cv2.imwrite("segmentation_output_img1.jpg", segmented) 
+cv2.imwrite("Question1Outputs/segmentationOutputImg5.jpg", segmented) 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
@@ -40,7 +43,7 @@ cv2.drawContours(rgb, filtered_cnt, -1, (0, 255, 0), thickness=2)
 
 plt.imshow(rgb)
 plt.show()
-cv2.imwrite("coin_count_output_img1.jpg", rgb)  
+cv2.imwrite("Question1Outputs/coinCountOutputImg5.jpg", cv2.cvtColor(rgb, cv2.COLOR_RGB2BGR))  
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
